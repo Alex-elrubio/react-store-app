615ac18f81d310fe45f7b1e18422589e72b41e3f
Alex.ElRubio <larychev.port@gmail.com>
2022-10-26 00:10:03 +0300

    changes in footer.js, was changed link on gitlab web site

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer className={styles.footer}>
       <p>
-        <GetIcon icon="BsFillHeartFill" size={22} color="#da0037" /> <a href="https://github.com/melihsahtulek">melihsahtulek</a>
+        <GetIcon icon="BsFillHeartFill" size={22} color="#da0037" /> <a href="http://gitlab.a-level.com.ua/Alex.ElRubio">Larychev Oleksandr</a>
       </p>
     </footer>
   );
